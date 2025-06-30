bccca9fabd2a90f010f6964fd2002eee3089119f
Sanjay Kirupaharan <sanjaykirupaharan21@gmail.com>
2025-06-30 11:00:08 +0530

    style: change hero section background

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -2,16 +2,17 @@
 
 import Image from "next/image";
 import { Vortex } from "./ui/background-vortex";
+import { ShootingStars } from "./ui/background-shooting-stars";
+import { Star } from "lucide-react";
+import { StarsBackground } from "./ui/background-stars";
 
 type Props = {};
 export const HeroSection = ({ }: Props) => {
   return (
     <div className="h-screen w-full relative flex flex-col items-center justify-center antialiased overflow-hidden">
 
-      <Vortex
-        backgroundColor="black"
-        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
-      >
+      <StarsBackground className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"/>
+      <ShootingStars />
         <Image
           width={600}
           height={600}
@@ -19,7 +20,14 @@ export const HeroSection = ({ }: Props) => {
           alt="logo"
           className="w-80 lg:w-96 xl:scale-125 z-40"
         />
-      </Vortex>
+
+
+      {/* <Vortex
+        backgroundColor="black"
+        className="flex items-center flex-col justify-center px-2 md:px-10 py-4 w-full h-full"
+      >
+        
+      </Vortex> */}
 
       <Image
         width={4000}
