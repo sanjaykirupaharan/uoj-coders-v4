10eddfe070a12ff536436f9d7953d82c5b345feb
Sanjay Kirupaharan <sanjaykirupaharan21@gmail.com>
2025-08-07 14:42:49 +0530

    fix: fix minor bugs

--- components/finalists.tsx
+++ components/finalists.tsx
@@ -15,24 +15,20 @@ export default function Finalists() {
                 <h1 className=" text-2xl lg:text-4xl text-center font-bold bg-gradient-to-b from-[#18ccfc] via-[#6344f5] to-[#ae48ff] bg-clip-text text-transparent uppercase">🏆 The Finalists</h1>
                 <p className="text-center mt-2 text-xs lg:text-lg">Witness the showdown on August 23rd 2025!</p>
             </div>
-            <div className="w-3/4 lg:w-7/12 mx-auto mt-10 overflow-y-hidden">
-                <Table className="overflow-y-hidden ">
-                    <TableCaption>
-                        The list is ordered according to the alphabetical order of the Team names.
-                    </TableCaption>
-                    <div className="grid grid-cols-1 md:grid-cols-4 gap-10 p-4 font-light">
-                        {teams.map((team) => (
-                            <div className="bg-gradient-to-b from-[#040210] via-[#2c1375] to-[#ae48ff] p-6 rounded-xl shadow-md hover:shadow-lg transition-all hover:scale-[1.02]">
-                                <h3 className="text-xl font-semibold text-center mt-1 text-white">{team.teamName}</h3>
-                                <p className="text-gray-300 text-center">{team.university}</p>
-                                {/* <div className="mt-3 flex justify-center space-x-2">
+            <div className="w-3/4 lg:w-7/12 mx-auto mt-10 overflow-y-hidden text-center">
+                The list is ordered according to the alphabetical order of the Team names.
+                <div className="grid grid-cols-1 md:grid-cols-4 gap-10 p-4 font-light">
+                    {teams.map((team) => (
+                        <div className="bg-gradient-to-b from-[#040210] via-[#2c1375] to-[#ae48ff] p-6 rounded-xl shadow-md hover:shadow-lg transition-all hover:scale-[1.02]">
+                            <h3 className="text-xl font-semibold text-center mt-1 text-white">{team.teamName}</h3>
+                            <p className="text-gray-300 text-center">{team.university}</p>
+                            {/* <div className="mt-3 flex justify-center space-x-2">
                                     <span className="bg-silver px-3 py-1 rounded-full text-sm">🔥 3 Wins</span>
                                     <span className="bg-platinum px-3 py-1 rounded-full text-sm">🎯 Top 5%</span>
                                 </div> */}
-                            </div>
-                        ))}
-                    </div>
-                </Table>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
