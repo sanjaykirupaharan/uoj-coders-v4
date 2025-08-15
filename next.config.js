889c6f10ce5ce33556f02918552951fa5e83fa31
Sanjay Kirupaharan <sanjaykirupaharan21@gmail.com>
2025-08-15 22:27:42 +0530

    refactor: remove(temporarily) finalists section

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  assetPrefix: '/',
+  // assetPrefix: '/',
   trailingSlash: true,
   images: {
     remotePatterns: [
